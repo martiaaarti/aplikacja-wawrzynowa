a7f51d24f4af027fdf48cb1efa688a8039ebd070
DESKTOP-VF4GDHS\Martyna <martiaaarti@gmail.com>
2018-10-21 16:32:40 +0200

    Facebook and Youtube links adding

--- imports/ui/Footer.js
+++ imports/ui/Footer.js
@@ -16,8 +16,8 @@ export default class Footer extends Component {
                     <Row>
                         <Col sm="12" md="12" lg="12">
                             <p className="align-self-center">&copy; 2018. Duszpasterstwo Akademickie Wawrzyny
-                                <FontAwesomeIcon icon={['fab', 'facebook']} size="2x" pull="right" />
-                                <FontAwesomeIcon icon={['fab', 'youtube']} size="2x" pull="right" />
+                                <a className="facebook-icon" href="https://www.facebook.com/DAWawrzyny/"> <FontAwesomeIcon icon={['fab', 'facebook']} size="2x" pull="right" color="rgba(0,0,0,.5)" /></a>
+                                <a className="youtube-icon" href="https://www.youtube.com/user/DAWawrzyny"> <FontAwesomeIcon icon={['fab', 'youtube']} size="2x" pull="right" color="rgba(0,0,0,.5)"/></a>
                             </p>
                         </Col>
                     </Row>
