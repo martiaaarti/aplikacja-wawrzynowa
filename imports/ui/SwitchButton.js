368383308b7f0f0f23146fab6fd1ebc15ff41cdf
DESKTOP-VF4GDHS\Martyna <martiaaarti@gmail.com>
2018-10-31 20:14:08 +0100

    Switch update

--- imports/ui/SwitchButton.js
+++ imports/ui/SwitchButton.js
@@ -2,18 +2,11 @@ import React, { Component } from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
 
 
-export default class Switch extends Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-    render() {
-        return (
-            <ButtonGroup style={{ marginTop: '15px', marginBottom: '12px' }}>
-                <Button color="secondary">Lista</Button>
-                <Button color="secondary">Kalendarz</Button>
-            </ButtonGroup>
-        )
-    }
-}
+const Switch = props => {
+    return (
+        <ButtonGroup style={{ marginTop: '15px', marginBottom: '12px' }}>
+            <Button color="secondary">Lista</Button>
+            <Button color="secondary">Kalendarz</Button>
+        </ButtonGroup>
+    );
+};
