e2f4a43f1205d769ee43296e1998dfcbb4130717
DESKTOP-VF4GDHS\Martyna <martiaaarti@gmail.com>
2018-11-01 16:55:05 +0100

    Update

--- imports/ui/EventPanel.js
+++ imports/ui/EventPanel.js
@@ -7,21 +7,10 @@ import { Collapse, Button, CardBody, Card } from 'reactstrap';
 export default class EventPanel extends Component {
     constructor(props) {
         super(props);
-        this.onEnter = this.onEnter.bind(this);
-        this.onExit = this.onExit.bind(this);
         this.toggle = this.toggle.bind(this);
         this.state = { collapse: false };
     }
-
-    onEnter() {
-        this.setState({ status: <FontAwesomeIcon icon="caret-down" pull="left" size="lg" /> });
-    }
-
-    onExit() {
-        this.setState({ status: <FontAwesomeIcon icon="caret-right" pull="left" size="lg" /> });
-    }
-
-
+   
     toggle() {
         this.setState({ collapse: !this.state.collapse });
     }
@@ -31,17 +20,15 @@ export default class EventPanel extends Component {
         const { eventType } = this.props
 
         return (
-            <Card id="main-card">
+            <Card id="event-panel-main-card">
                 <CardBody>
                     <Button block color="secondary" size="lg" onClick={this.toggle} style={{ marginBottom: '0.5rem', textAlign: 'left' }}>
                         <span>
                             <FontAwesomeIcon icon={this.state.collapse ? "caret-right" : "caret-down"} pull="left" size="lg" style={{ paddingRight: '5px' }} />
                         </span>
-                        {eventType}
-                        </Button>
-                    <Collapse isOpen={this.state.collapse}
-                        onEnter={this.onEnter}
-                        onExit={this.onExit}>
+                    {eventType}
+                    </Button>
+                    <Collapse isOpen={this.state.collapse}>
                         <Card style={{ paddingTop: '0px'}}>
                             <CardBody style={{ paddingBottom: '10px', paddingTop: '10px' }}>Poranne siódemki</CardBody>
                         </Card>
