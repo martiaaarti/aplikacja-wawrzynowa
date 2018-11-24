6f8199fe7c04aaa57bd41d5915980fcf140bb833
DESKTOP-VF4GDHS\Martyna <martiaaarti@gmail.com>
2018-11-24 11:59:44 +0100

    Ministry updated

--- imports/api/ministry.js
+++ imports/api/ministry.js
@@ -4,18 +4,36 @@ import SimpleSchema from 'simpl-schema';
 export const Ministry = new Mongo.Collection('ministry');
 
 const dataModelMinistry = new SimpleSchema({
-    
+
     id: { type: SimpleSchema.Integer },
     admina: { type: SimpleSchema.Integer },
-    name: {type: String},
+    name: { type: String },
     description: String,
-    color: {type: String},
+    color: { type: String },
+
     volunteerEvents: {
         type: Array
     },
     'volunteerEvents.$': {
         type: SimpleSchema.Integer
+    },
+
+    recurringEvents: {
+        type: Array,
+        optional: true
+    },
+    'recurringEvents.$': {
+        type: SimpleSchema.Integer
+    },
+
+    singleEvents: {
+        type: Array,
+        optional: true
+    },
+    'singleEvents.$': {
+        type: SimpleSchema.Integer
     }
+
 });
 
 Ministry.attachSchema(dataModelMinistry);
