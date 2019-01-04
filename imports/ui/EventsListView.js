6ad00856f323f1faabed0abf2a67109443fec055
DESKTOP-VF4GDHS\Martyna <martiaaarti@gmail.com>
2019-01-04 20:48:53 +0100

    EventListPanel small change

--- imports/ui/EventsListView.js
+++ imports/ui/EventsListView.js
@@ -4,24 +4,40 @@ import EventPanel from './EventPanel.js';
 import Switch from './SwitchButton.js';
 import PropTypes from 'prop-types';
 
+const eventTypes = [
+    {
+        title: "Wydarzenia pojedyncze",
+        id: "single-events"
+    },
+    {
+        title: "Wydarzenia cykliczne",
+        id:"recurring-events"
+    }
+];
 
 export default class EventsListView extends Component {
     constructor(props) {
         super(props);
-    }
+    }    
 
     render() {
 
         return (
             <div>
                 <div className="d-flex justify-content-end"><Switch /></div>
-                <EventPanel id="single-events" eventType='Wydarzenia pojedyncze' events={this.props.events} />
-                <EventPanel id="recurring-events" eventType='Wydarzenia cykliczne' events={this.props.events} />
+                {eventTypes.map((eventType) => (
+                    <EventPanel 
+                    key = {eventType.id} 
+                    eventType={eventType.title}                    
+                    events={this.props.events} 
+                    />
+
+                ))}
             </div>
         )
     }
 }
 
 EventsListView.propTypes = {
-    events: PropTypes.array.isRequired,
+    events: PropTypes.array.isRequired
 };
