b8b4c627c15b71efd1cf1b04b2bbe75b2e6ed26f
DESKTOP-VF4GDHS\Martyna <martiaaarti@gmail.com>
2019-01-06 17:22:05 +0100

    Problem with SwitchButton

--- imports/ui/EventsView.js
+++ imports/ui/EventsView.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
-import { Route } from 'react-router-dom';
 
 import EventsListView from './EventsListView.js';
 import EventsCalendarView from './EventsCalendarView.js';
-import Switch from './SwitchButton.js';
-import { Button, ButtonGroup } from 'reactstrap';
+import SwitchButton from './SwitchButton.js';
 
 
 
@@ -17,7 +15,7 @@ export default class EventsView extends Component {
         };
     }  
 
-    onNavigate(isCalendarSelected) {
+    onNavigate(isCalendarSelected){
         this.setState({isCalendarSelected});
     }
     
@@ -25,12 +23,14 @@ export default class EventsView extends Component {
     render() {
         return (         
                      <div>
-                         <div className="d-flex justify-content-end">
+                         {/* <div className="d-flex justify-content-end">
                             <ButtonGroup className='my-4'>
                                 <Button color="secondary" onClick={() => this.onNavigate(false)} active={this.state.isCalendarSelected === false}>Lista</Button>
                                 <Button color="secondary" onClick={() => this.onNavigate(true)} active={this.state.isCalendarSelected === true}>Kalendarz</Button>
                             </ButtonGroup>
-                        </div>                         
+                        </div>                          */}
+
+                        <SwitchButton onNavigate={this.onNavigate} isCalendarSelected={this.state.isCalendarSelected}/>
 
                         {this.state.isCalendarSelected === false && (
                             <EventsListView/>
--- imports/ui/SwitchButton.js
+++ imports/ui/SwitchButton.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
 
 
-export default class Switch extends Component {
+export default class SwitchButton extends Component {
     constructor(props) {
         super(props);       
     }
 
-
-
     render() {
         return (
-            <ButtonGroup className='my-4'>
-                <Button color="secondary" onClick={this.props.onNavigate(false)} active={this.state.isCalendarSelected === false}>Lista</Button>
-                <Button color="secondary" onClick={this.props.onNavigate(true)} active={this.state.isCalendarSelected === true}>Kalendarz</Button>
-            </ButtonGroup>
+            <div className="d-flex justify-content-end">
+                <ButtonGroup className='my-4'>
+                    <Button color="secondary" onClick={() => this.props.onNavigate(false)} active={this.state.isCalendarSelected === false}>Lista</Button>
+                    <Button color="secondary" onClick={() => this.props.onNavigate(true)} active={this.state.isCalendarSelected === true}>Kalendarz</Button>
+                </ButtonGroup>
+            </div>             
         )
     }
 }
