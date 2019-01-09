17a4f92e8d33a04dabb397fffd0cfa7706701bd3
DESKTOP-VF4GDHS\Martyna <martiaaarti@gmail.com>
2019-01-09 10:36:06 +0100

    small change

--- imports/db/events/temporaryDB.js
+++ imports/db/events/temporaryDB.js
@@ -5,7 +5,7 @@ Meteor.startup(() => {
     countEvents = Events.find().count();
 
     if (countEvents === 0) {
-        ({
+        Events.insert({
             id: 000,
             name: 'Wawrzynowe Zaduszki',
             description: 'Wawrzyny i Wawrzynki! Zapraszamy Was serdecznie na Wawrzynowe Zaduszki.',
@@ -14,7 +14,7 @@ Meteor.startup(() => {
             duration: 3,
             attendeesLimit: 150       
         });
-        ({
+        Events.insert({
             id: 001,
             name: 'Spotkanie Redakcji "Wawrzyna"',
             description: 'Nowy rok, starzy my i... nowy numer na horyzoncie! Czas więc, żeby o nim porozmawiać, a przede wszystkim: trochę się zintegrować. Nieważne, czy jesteś stałym bywalcem czy może dopiero zastanawiasz się, czy redakcja to miejsce dla Ciebie – wpadnij! :)',
@@ -22,7 +22,7 @@ Meteor.startup(() => {
             startDateTime: '2019-01-15T06:00:00+02:00',
             duration: 2
         });
-        ({
+        Events.insert({
             id: 002,
             name: 'Bal Na 200 Par 2019',
             description: 'Jak co roku, od niepamiętnych już czasów, mamy zaszczyt zaprosić Was na doroczny Bal Duszpasterstw Akademickich...',
@@ -30,7 +30,7 @@ Meteor.startup(() => {
             startDateTime: '2019-02-16T06:00:00+04:00',
             attendeesLimit: 200
         });
-        ({
+        Events.insert({
             id: 003,
             name: 'Wykład Patriotyczny - Powstanie Wielkopolskie',
             description: 'Zapraszamy na ostatnie spotkanie w tym roku kalendarzowym. Tematem wykładu będzie Powstanie Wielkopolskie. Do zobaczenia!',
