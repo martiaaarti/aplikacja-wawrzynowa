057990b1416965c69ac3b475a19ef402e950f419
Martyna <martiaaarti@gmail.com>
2019-03-28 09:24:31 +0100

    Collection Shortcut

--- imports/api/db/index.js
+++ imports/api/db/index.js
@@ -1,9 +1,14 @@
 import Events from './events/collection';
 import Ministries from './ministries/collection';
+import SingleEvents from './singleEvents/collection';
+import RecurringEvents from './recurringEvents/collection';
+
 import './links.js';
 import './temporaryDBs';
 
 export {
     Events,
-    Ministries
+    Ministries,
+    SingleEvents,
+    RecurringEvents
 }
